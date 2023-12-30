947312789ca9d4ccec34307702fd1fd2258214b6
Justin <justinjohnsonprogrammer@outlook.com>
2023-12-29 23:18:48 -0500

    Updated taskRoutes with CRUD operations

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../models/taskModel'); // Adjust the path as necessary
+const Task = require("../models/taskModel"); // Adjust the path as necessary
 
 // POST: Create a new task
-router.post('/tasks', async (req, res) => {
+router.post("/tasks", async (req, res) => {
   try {
     const task = new Task(req.body);
     await task.save();
@@ -14,7 +14,7 @@ router.post('/tasks', async (req, res) => {
 });
 
 // GET: Retrieve all tasks
-router.get('/tasks', async (req, res) => {
+router.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).send(tasks);
@@ -22,7 +22,37 @@ router.get('/tasks', async (req, res) => {
     res.status(500).send(error);
   }
 });
+// PATCH: Update a specific task by ID
+router.patch("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-// additional routes for updating and deleting tasks will go here
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// DELETE: Delete a specific task by ID
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
